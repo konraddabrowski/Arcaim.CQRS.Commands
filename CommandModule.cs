05111b13f0c155d595279c99f43e2f472e0d757c
Konrad Dąbrowski <konrad.dabrowski@arcaim.pl>
2020-10-31 14:07:00 +0100

    Fixed bug assembly

--- CommandModule.cs
+++ CommandModule.cs
@@ -3,15 +3,13 @@ using Autofac;
 
 namespace Arcaim.CQRS.Commands
 {
-    public class CommandModule : Autofac.Module
+    public abstract class CommandModule : Autofac.Module
     {
+        protected abstract Assembly GetAssembly();
+
         protected override void Load(ContainerBuilder builder)
         {
-            var assembly = typeof(CommandModule)
-                .GetTypeInfo()
-                .Assembly;
-
-            builder.RegisterAssemblyTypes(assembly)
+            builder.RegisterAssemblyTypes(GetAssembly())
                 .AsClosedTypesOf(typeof(ICommandHandler<>))
                 .InstancePerLifetimeScope();
 
