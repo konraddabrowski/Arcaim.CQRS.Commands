744201514b0ff395eaa1c83b9a796f36a6d2aa99
Konrad Dąbrowski <konrad.dabrowski@arcaim.pl>
2020-11-17 23:26:00 +0100

    The Assembly from Directory DLL was implemented

--- Extensions.cs
+++ Extensions.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Arcaim.CQRS.Commands
@@ -8,12 +12,20 @@ namespace Arcaim.CQRS.Commands
         public static IServiceCollection AddCommandHandlers(this IServiceCollection services)
         {
             services.AddSingleton<ICommandDispatcher, CommandDispatcher>();
-            services.Scan(s => s.FromAssemblies(AppDomain.CurrentDomain.GetAssemblies())
-                    .AddClasses(c => c.AssignableTo(typeof(ICommandHandler<>)))
-                    .AsImplementedInterfaces()
-                    .WithTransientLifetime());
+
+            GetAssemblies().SelectMany(assembly => assembly.GetTypes()
+                .Where(type => !type.IsInterface && !type.IsAbstract && type.GetInterfaces()
+                .Any(y => y.Name.Equals(typeof(ICommandHandler<>).Name, StringComparison.InvariantCulture))))
+                .ToList().ForEach(type => type.GetInterfaces()
+                .ToList().ForEach(implementedInterfaces =>
+                    services.AddTransient(implementedInterfaces, type)
+                ));
 
             return services;
         }
+
+        private static IEnumerable<Assembly> GetAssemblies()
+            => Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll")
+                .Select(x => Assembly.Load(AssemblyName.GetAssemblyName(x)));
     }
 }
