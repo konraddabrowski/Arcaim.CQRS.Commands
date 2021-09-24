7ccd33afc01a0e49c7e5927e22e48a03227d8653
Konrad Dąbrowski <konrad.dabrowski.90@gmail.com>
2021-09-24 13:38:17 +0200

    Update

--- ICommandDispatcher.cs
+++ ICommandDispatcher.cs
@@ -2,7 +2,7 @@ using System.Threading.Tasks;
 
 namespace Arcaim.CQRS.Commands
 {
-    internal interface ICommandDispatcher
+    public interface ICommandDispatcher
     {
         Task DispatchAsync<T>(T command) where T : class, ICommand;
     }
